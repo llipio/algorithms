09e8aeac4461268f9a1d0b5e894acafe94360d04
Manik Sachdeva <msach22@gmail.com>
2017-04-26 21:46:27 -0700

    add true and false from chai

--- test/16.js
+++ test/16.js
@@ -5,14 +5,14 @@ const hasDuplicates = duplicates.hasDuplicates;
 describe('check duplicates', () => {
   it('should return true since array has duplicates', () => {
     const array = [1,2,3,3,4];
-    expect(hasDuplicates(array)).eql(true);
+    expect(hasDuplicates(array)).to.be.true;
   });
   it('should return false because array does not have duplicates', () => {
     const array = [1,2,3,4];
-    expect(hasDuplicates(array)).eql(false);
+    expect(hasDuplicates(array)).to.be.false;
   });
   it('should return false because array is empty', () => {
     const array = [];
-    expect(hasDuplicates(array)).eql(false);
+    expect(hasDuplicates(array)).to.be.false;
   });
 });
