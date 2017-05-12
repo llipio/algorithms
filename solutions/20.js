34447f67b13d38cff224c5adcdfed5418eb886ee
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-11 18:56:13 -0700

    remove extraneous comments

--- solutions/20.js
+++ solutions/20.js
@@ -11,15 +11,10 @@
  * @returns {Number} largestSum - the largestSum of two consecutive numbers in arr
  */
 const solution = arr => {
-    // Initialize largestSum to the sum of the first two elements in arr
     let largestSum = arr[0] + arr[1];
 
-    // Loop through arr, adding i + (i + 1) and compare to largestSum, updating as necessary
     for (let i = 1; i < arr.length; i++) {
-        // Set the current sum of the iteration to the current element i and the next element, i + 1
         let currentSum = arr[i] + arr[i + 1];
-
-        // If currentSum is larger than largestSum, update accordingly, else leave as is
         largestSum = currentSum > largestSum ? currentSum : largestSum;
     }
 
