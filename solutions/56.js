e38e356502c203fe4bb2b6f1121cc983597714d9
Yoo Jin Lim <yjlim5@gmail.com>
2017-06-02 15:10:36 -0700

    issue #56 add header comments to the solution function

--- solutions/56.js
+++ solutions/56.js
@@ -1,4 +1,17 @@
 // coin denomination problem
+// Find minimum number of coins required to make up a given value
+// Input: [5,10,25], 100
+// Output: 4
+
+// Solution by Yoo Jin
+
+/**
+ * Returns the minimum number of coins required to make up the given total value
+ * Each coin denomination can be used as many times as needed.
+ * @param {number[]} arr - An array of coin denomination numbers
+ * @param {number} total - The total value 
+ * @returns {number} min - The mininum number of coins which add up to the toal value
+ */
 
 const solution = (arr, total) => {
   if (total === 0) {
