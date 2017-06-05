e4fef172d84c69a1588ff89aeb3e7ba8f6288894
Colin Xie <colinxie99@gmail.com>
2017-06-04 18:59:51 -0700

    updated syntax and comments #59

--- solutions/59.js
+++ solutions/59.js
@@ -1,10 +1,9 @@
-// Finding prime numbers.
 // Takes in a number, returns true if the number is prime, false otherwise.
 // input: 1
 // output: false
 
 // Solution by Colin Xie @ColinX13
-/*
+/**
  * Returns true or false based on whether the number is prime or not prime
  * @param {number} num - a number that is being input
  * @return {boolean} isPrime - the number is prime
