d3c2b16c453a28c3c8a564eba8ca7f0197843cc1
seemcat <hello@vimgirl.com>
2017-06-07 07:57:37 -0700

    Modified test file for #8

--- test/8.js
+++ test/8.js
@@ -1,12 +1,7 @@
 const expect = require('chai').expect;
 let solution = require('../solutions/8').solution;
-<<<<<<< HEAD
-solution = require('../yourSolution').solution;
-=======
-let solution1 = require('../solutions/8').solution;
-
+let solution1 = require('../solutions/8').solution1;
 // solution = require('./yourSolution').solution;
->>>>>>> Necessary Changes
 
 describe('reverse String', () => {
   it('should reverse a string in reverse', () => {
@@ -29,4 +24,25 @@ describe('reverse String', () => {
     const expected = "welcome emoclew";
     expect(solution(actual)).to.equal(expected);
   }); 
+  it('should reverse a string in reverse', () => {
+    const actual = "react";
+    const expected = "tcaer";
+    expect(solution1(actual)).to.equal(expected);
+  });
+  it('should reverse a string in reverse', () => {
+    const actual = "word";
+    const expected = "drow";
+    expect(solution1(actual)).to.equal(expected);
+  });
+ it('should reverse a string in reverse', () => {
+    const actual = "I";
+    const expected = "I";
+    expect(solution1(actual)).to.equal(expected);
+  });
+ it('should reverse a string in reverse', () => {
+    const actual = "welcome emoclew";
+    const expected = "welcome emoclew";
+    expect(solution1(actual)).to.equal(expected);
+  }); 
+ 
 });
