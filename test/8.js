2bd1d1989e4058ed39db94329a8ded5bfa471fbf
Colin Xie <colinxie99@gmail.com>
2017-06-09 09:19:57 -0700

    additional test cases for #8

--- test/8.js
+++ test/8.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 let solution = require('../solutions/8').solution;
-// solution = require('./yourSolution').solution;
+solution = require('../yourSolution').solution;
 
 describe('reverse String', () => {
   it('should reverse a string in reverse', () => {
@@ -8,4 +8,19 @@ describe('reverse String', () => {
     const expected = "tcaer";
     expect(solution(actual)).to.equal(expected);
   });
+ it('should reverse a string in reverse', () => {
+    const actual = "word";
+    const expected = "drow";
+    expect(solution(actual)).to.equal(expected);
+  });
+ it('should reverse a string in reverse', () => {
+    const actual = "I";
+    const expected = "I";
+    expect(solution(actual)).to.equal(expected);
+  });
+ it('should reverse a string in reverse', () => {
+    const actual = "welcome emoclew";
+    const expected = "welcome emoclew";
+    expect(solution(actual)).to.equal(expected);
+  }); 
 });
