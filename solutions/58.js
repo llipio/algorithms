c6e25ef89faa71233f71f0b1e17201ceb6715b2b
Yoo Jin Lim <yjlim5@gmail.com>
2017-06-14 10:56:41 -0700

    remove unnecessary newline

--- solutions/58.js
+++ solutions/58.js
@@ -5,12 +5,6 @@
 
 // Solution by Yoo Jin
 
-/**
- * Given an Employee, return an array of employees that creates the maximum fun.
- * There is one condition: An Employee cannot be on the guestlist if his direct manager is going to the party.
- * @param {Employee} emp - an Employee with fun value and a list of his staff
- * @returns {Employee []]} list - Array of employees that creates the maximum fun
- */
 class Employee {
   constructor(funVal, staffArray=[]) {
     this.funVal = funVal;
@@ -25,7 +19,12 @@ class Employee {
   }
 }
 
-
+/**
+ * Given an Employee, return an array of employees that creates the maximum fun.
+ * There is one condition: An Employee cannot be on the guestlist if his direct manager is going to the party.
+ * @param {Employee} emp - an Employee with fun value and a list of his staff
+ * @returns {Employee []]} list - Array of employees that creates the maximum fun
+ */
 const solution = (emp) => {
   if(!emp) {
     return [];
