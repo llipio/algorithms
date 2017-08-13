130794d4ff8e8a7cdfcc5f621634be94565af319
Daniel <drsoper@gmail.com>
2017-08-12 20:01:58 -0700

    min coin solution

--- test/56.js
+++ test/56.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const solution = require('../solutions/56.js').solution;
+const solution2 = require('../solutions/56.js').solution2;
 // solution = require('../yourSolution').solution;
 
 describe('return smallest number of coins required to return the change',
@@ -18,6 +19,21 @@ describe('return smallest number of coins required to return the change',
   });
   it('hard case [1,2,3,6,7], total=12', () => {
     expect(solution([1, 2, 3, 6, 7], 12)).to.equal(2);
+  });
+  it('simplest case [1], total=1', () => {
+    expect(solution2([1], 1)).to.equal(1);
+  });
+  it('simplest case [1,2], total=0', () => {
+    expect(solution2([1, 2], 0)).to.equal(0);
+  });
+  it('simple case [1,2], total=2', () => {
+    expect(solution2([1, 2], 2)).to.equal(1);
+  });
+  it('simple case [1,2], total=4', () => {
+    expect(solution2([1, 2], 4)).to.equal(2);
+  });
+  it('hard case [1,2,3,6,7], total=12', () => {
+    expect(solution2([1, 2, 3, 6, 7], 12)).to.equal(2);
   });
 });
 
